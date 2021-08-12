0e73029af45d2cc34faaef837cf330cd91ed5022
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-08-12 16:58:19 +0630

    inject FoodRecipesApi to RemoteDataSource

--- app/src/main/java/com/norm/foodrecipes/RemoteDataSource.kt
+++ app/src/main/java/com/norm/foodrecipes/RemoteDataSource.kt
@@ -1,6 +1,19 @@
 package com.norm.foodrecipes
 
-class RemoteDataSource(
+import com.norm.foodrecipes.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
 
+/**
+ * Data source from remote.
+ */
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
 ) {
+    /**
+     * Get all recipes via [FoodRecipesApi]
+     */
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
