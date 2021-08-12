c32fb72f73fc198b6cad429a78874a0d6edf39e3
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-08-12 15:34:46 +0630

    design shimmer placeholder layout

--- app/src/main/java/com/norm/foodrecipes/RecipesFragment.kt
+++ app/src/main/java/com/norm/foodrecipes/RecipesFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.norm.foodrecipes.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private var _binding: FragmentRecipesBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+
+        binding.recipesRecyclerView.showShimmer()
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
