d9ab50a794a7a6961bb87eb2e4c98ea90e9595d5
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-08-12 18:57:55 +0630

    create RecipesAdapter and binding data to row layout

---
+++ app/src/main/java/com/norm/foodrecipes/util/RecipesDiffUtil.kt
@@ -0,0 +1,25 @@
+package com.norm.foodrecipes.util
+
+import androidx.recyclerview.widget.DiffUtil
+
+class RecipesDiffUtil<E>(
+    private val oldList: List<E>,
+    private val newList: List<E>
+): DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
