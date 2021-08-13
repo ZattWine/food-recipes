fd4fd87c6e40c813e90124fa3e2099e0e31fadc5
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-08-13 22:42:15 +0630

    InstructionsFragment functionality coded

--- app/src/main/java/com/norm/foodrecipes/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/norm/foodrecipes/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.norm.foodrecipes.R
+import com.norm.foodrecipes.databinding.FragmentInstructionsBinding
+import com.norm.foodrecipes.models.Result
+import com.norm.foodrecipes.util.Constants
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_BUNDLE_KEY)
+
+        binding.instructionWebView.webViewClient = object : WebViewClient() {}
+        val sourceUrl: String = myBundle!!.sourceUrl
+        binding.instructionWebView.loadUrl(sourceUrl)
+
+        return binding.root
     }
 }
